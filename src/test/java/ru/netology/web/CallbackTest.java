ae663e4d9da04e50bb4008669aa9c09cd2f5bc83
Ramzesito <roman_shv@mail.ru>
2023-08-07 21:27:00 +0300

    updated test вгу ещ Ñdue to comments

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
@@ -11,16 +12,15 @@ import static com.codeborne.selenide.Selenide.*;
 public class CallbackTest {
 
     @Test
-    public void shouldTestSubmitForm() throws InterruptedException {
-        Configuration.headless = true;
+    public void shouldTestSubmitForm() {
+        //Configuration.headless = true; // оставил здесь под комментом, чтобы не забыть
+
         open("http://localhost:9999");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Василий Петров");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79876543210");
         $(By.cssSelector("[data-test-id=agreement]")).click();
         $(By.className("button")).click();
-        String gotText = $(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", gotText);
+        $(By.cssSelector("[data-test-id=order-success]")).shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
-        //Thread.sleep(5000);
     }
 }
